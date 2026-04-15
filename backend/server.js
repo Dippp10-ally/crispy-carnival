e03cbb4e1d248ad65bfcb16ac447bf051ca4fe7b
Dippp10-ally <dipikabarkat@gmail.com>
2026-04-15 21:39:21 +0400

    Delete backend/server.js

--- backend/server.js
+++
@@ -1,1 +0,0 @@
-const express = require('express'); const cors = require('cors'); require('dotenv').config(); const app = express(); app.use(cors()); app.use(express.json()); const cardRoutes = require('./routes/cardRoutes'); app.use('/api', cardRoutes); const PORT = process.env.PORT || 3001; app.listen(PORT, () => { console.log(`🚀 Server running on http://localhost:${PORT}`); });
